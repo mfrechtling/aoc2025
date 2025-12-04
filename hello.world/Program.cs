81fec56cf38443d6e8d6bf98f6aff1aac6f63693
Michael Frechtling <michael@frechtling.net>
2025-12-04 17:55:39 +1100

    Add dummy projects with unit tests and create dotNet build and test workflow (#2)
    Co-authored-by: Michael Frechtling <mfrechtling@gmail.com>

---
+++ hello.world/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace hello.world
+{
+    public class GreetingGenerator
+    {
+        public string GenerateGreeting()
+        {
+            return "Hello, World!";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var generator = new GreetingGenerator();
+            // This now just runs the function
+            Console.WriteLine(generator.GenerateGreeting());
+        }
+    }
+}
